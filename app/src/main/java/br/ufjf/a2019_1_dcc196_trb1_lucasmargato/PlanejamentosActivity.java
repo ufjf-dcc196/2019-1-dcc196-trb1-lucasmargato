2a040dc8320f6d1755dbfe536587af366d996124
Lucas <lmladeira@gmail.com>
2019-04-24 20:24:25 -0300

    Atividade principal renomeada

--- app/src/main/java/br/ufjf/a2019_1_dcc196_trb1_lucasmargato/PlanejamentosActivity.java
+++ app/src/main/java/br/ufjf/a2019_1_dcc196_trb1_lucasmargato/PlanejamentosActivity.java
@@ -3,11 +3,11 @@ package br.ufjf.a2019_1_dcc196_trb1_lucasmargato;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PlanejamentosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_planejamentos);
     }
 }
